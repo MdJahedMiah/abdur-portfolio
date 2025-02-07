ddfd81e9ee15a0e150071668e6bae15b59f5fd8c
Md Jahed Miah <mm22aif@herts.ac.uk>
2025-02-07 01:44:03 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<h1 className='text-3xl font-bold underline'>Seo Abdur</h1>
+<p>dsigital marketing</p>
+    </>
+  )
+}
+
+export default App
