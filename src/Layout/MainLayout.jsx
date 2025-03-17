eaa6cb927ea3950256027630f44470a8f765310d
Md Jahed Miah <mm22aif@herts.ac.uk>
2025-03-17 00:14:40 +0000

    navbar name is fix in layout

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer/Footer";
-import Navbar from "../components/Navbar/NavbarTemp";
+import Navbar from "../components/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
